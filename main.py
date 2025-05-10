74e47cfc30e362bd63459a0be31a3a3861e92749
sudharsfinale <sudharsfinale@gmail.com>
2025-05-10 21:01:55 +0530

    main.py file will have notes

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+- Process for problem solving
+- Read the problem twice to understand it - ask clarifying questions)
+- Try think basically of different ways to solve problem
+- Think end-to-end (E2E) of the best solutions based on complexity
+- Write the Algorithm from patterns in drawing
+- Code it out
+- Try and Improve it once you think you're finished
+- Go through other solutions (even if you answered correctly).
+_ Code should always be readable, ignore one liners
+"""
